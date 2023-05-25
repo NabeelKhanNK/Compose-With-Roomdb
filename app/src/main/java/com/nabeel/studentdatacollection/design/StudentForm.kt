6b4f3cf540d521724116cc87cac4646052f894ad
nabeel <nabeelunitykhan@gmail.com>
2023-05-25 12:16:17 +0530

    Toolbar added

--- app/src/main/java/com/nabeel/studentdatacollection/design/StudentForm.kt
+++ app/src/main/java/com/nabeel/studentdatacollection/design/StudentForm.kt
@@ -3,15 +3,21 @@ package com.nabeel.studentdatacollection.design
 import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import com.nabeel.studentdatacollection.Screen
 import com.nabeel.studentdatacollection.database.dao.StudentDao
 import com.nabeel.studentdatacollection.database.entity.StudentInfo
+import com.nabeel.studentdatacollection.ui.theme.Purple700
 import com.nabeel.studentdatacollection.widgets.CheckBoxUI
 import com.nabeel.studentdatacollection.widgets.RadioButtonUi
 import com.nabeel.studentdatacollection.widgets.SpacerUI
@@ -21,7 +27,7 @@ import kotlinx.coroutines.launch
 @Composable
 fun StudentForm(studentDao: StudentDao, navController: NavHostController) {
     val scaffoldState = rememberScaffoldState()
-    val mcontext = LocalContext.current
+    val mContext = LocalContext.current
     var name by remember {
         mutableStateOf("")
     }
@@ -31,51 +37,64 @@ fun StudentForm(studentDao: StudentDao, navController: NavHostController) {
         modifier = Modifier.fillMaxSize(),
         scaffoldState = scaffoldState
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp)
-        ) {
-            TextField(
-                value = name,
-                label = {
-                    Text(text = "Name")
+        Column() {
+            TopAppBar(title = {
+                Text(text = "Form", color = Color.White, textAlign = TextAlign.Center)
+            }, modifier = Modifier.align(CenterHorizontally),
+                backgroundColor = Purple700,
+                navigationIcon = {
+                    IconButton(onClick = { }) {
+                        Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "Menu Btn", tint = Color.White)
+                    }
                 },
-                onValueChange = {
-                    name = it
-                },
-                singleLine = true,
-                modifier = Modifier.fillMaxWidth()
-            )
-            SpacerUI()
-            Text(text = "Gender")
-            SpacerUI()
-            val gender = RadioButtonUi(scaffoldState)
-            SpacerUI()
-            Text(text = "Class")
-            SpacerUI()
-            val inClass = SpinnerUI()
-            SpacerUI()
-            Text(text = "Subjects")
-            SpacerUI()
-            val subjects = CheckBoxUI(scaffoldState = scaffoldState)
-            SpacerUI()
-            Row(modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center) {
-                Button(
-                    modifier = Modifier.padding(8.dp),
-                    onClick = {
-                        val studentData = StudentInfo(0,name,gender,inClass,subjects)
-                        scope.launch {
-                            val id = studentDao.insertStudentInfo(studentData)
-                            var mssg: String  = ""
-                            mssg = if (id>0) "Add Successfully" else "Failed"
-                            Toast.makeText(mcontext, mssg, Toast.LENGTH_LONG).show()
-//                            navController.navigate(Screen.MainScreen.route)
-                            navController.backQueue
-                        }
-                    }) {
-                    Text(text = "Submit")
+//                backgroundColor = Color.Transparent,
+                contentColor = Color.Gray,
+                elevation = 2.dp)
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ) {
+                TextField(
+                    value = name,
+                    label = {
+                        Text(text = "Name")
+                    },
+                    onValueChange = {
+                        name = it
+                    },
+                    singleLine = true,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                SpacerUI()
+                Text(text = "Gender")
+                SpacerUI()
+                val gender = RadioButtonUi(scaffoldState)
+                SpacerUI()
+                Text(text = "Class")
+                SpacerUI()
+                val inClass = SpinnerUI()
+                SpacerUI()
+                Text(text = "Subjects")
+                SpacerUI()
+                val subjects = CheckBoxUI(scaffoldState = scaffoldState)
+                SpacerUI()
+                Row(modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center) {
+                    Button(
+                        modifier = Modifier.padding(8.dp),
+                        onClick = {
+                            val studentData = StudentInfo(0,name,gender,inClass,subjects)
+                            scope.launch {
+                                val id = studentDao.insertStudentInfo(studentData)
+                                var mssg: String  = ""
+                                mssg = if (id>0) "Add Successfully" else "Failed"
+                                Toast.makeText(mContext, mssg, Toast.LENGTH_LONG).show()
+                                navController.popBackStack()
+                            }
+                        }) {
+                        Text(text = "Submit")
+                    }
                 }
             }
         }
--- app/src/main/java/com/nabeel/studentdatacollection/design/StudentList.kt
+++ app/src/main/java/com/nabeel/studentdatacollection/design/StudentList.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -21,6 +22,7 @@ import androidx.navigation.NavHostController
 import com.nabeel.studentdatacollection.Screen
 import com.nabeel.studentdatacollection.database.dao.StudentDao
 import com.nabeel.studentdatacollection.database.entity.StudentInfo
+import com.nabeel.studentdatacollection.ui.theme.Purple700
 
 @Composable
 fun StudentList(navController: NavHostController, studentDao: StudentDao) {
@@ -37,68 +39,82 @@ fun StudentList(navController: NavHostController, studentDao: StudentDao) {
     Scaffold(
         modifier = Modifier.fillMaxSize()
     ) {
-        Box(modifier = Modifier.fillMaxSize()) {
-            LazyColumn{
-
-                itemsIndexed(
-                    items = al
-//                    items = listOf("Student 1","Student 2","Student 3","Student 4")
-                ) { _, String ->
-                    Card(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(horizontal = 16.dp, vertical = 8.dp),
-                        shape = RoundedCornerShape(6.dp),
-                        elevation = 4.dp
-                    ) {
-                        Column(modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(8.dp)) {
-                            Text(
-                                text = "Name : ${String.name}",
-                                fontSize = 16.sp,
-                                fontWeight = FontWeight.Bold,
-                                textAlign = TextAlign.Start,
-                                modifier = Modifier
-                                    .padding(8.dp)
-                            )
-                            Text(
-                                text = "Class : ${String.in_class}",
-                                fontSize = 16.sp,
-                                fontWeight = FontWeight.Bold,
-                                textAlign = TextAlign.Start,
-                                modifier = Modifier
-                                    .padding(8.dp)
-                            )
-                            Text(
-                                text = "Gender : ${String.gender}",
-                                fontSize = 16.sp,
-                                fontWeight = FontWeight.Bold,
-                                textAlign = TextAlign.Start,
-                                modifier = Modifier
-                                    .padding(8.dp)
-                            )
-                            Text(
-                                text = "Subjects : ${String.subjects.replace("[","").replace("]","")}",
-                                fontSize = 16.sp,
-                                fontWeight = FontWeight.Bold,
-                                textAlign = TextAlign.Start,
-                                modifier = Modifier
-                                    .padding(8.dp)
-                            )
-                        }
+        Column() {
+            TopAppBar(title = {
+                Text(text = "Form", color = Color.White, textAlign = TextAlign.Center)
+            }, modifier = Modifier.align(Alignment.CenterHorizontally),
+                backgroundColor = Purple700,
+                navigationIcon = {
+                    IconButton(onClick = { }) {
+                        Icon(imageVector = Icons.Filled.Menu, contentDescription = "Menu Btn", tint = Color.White)
                     }
+                },
+//                backgroundColor = Color.Transparent,
+                contentColor = Color.Gray,
+                elevation = 2.dp)
+            Box(modifier = Modifier.fillMaxSize()) {
+                LazyColumn{
 
+                    itemsIndexed(
+                        items = al
+//                    items = listOf("Student 1","Student 2","Student 3","Student 4")
+                    ) { _, String ->
+                        Card(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 16.dp, vertical = 8.dp),
+                            shape = RoundedCornerShape(6.dp),
+                            elevation = 4.dp
+                        ) {
+                            Column(modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(8.dp)) {
+                                Text(
+                                    text = "Name : ${String.name}",
+                                    fontSize = 16.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    textAlign = TextAlign.Start,
+                                    modifier = Modifier
+                                        .padding(8.dp)
+                                )
+                                Text(
+                                    text = "Class : ${String.in_class}",
+                                    fontSize = 16.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    textAlign = TextAlign.Start,
+                                    modifier = Modifier
+                                        .padding(8.dp)
+                                )
+                                Text(
+                                    text = "Gender : ${String.gender}",
+                                    fontSize = 16.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    textAlign = TextAlign.Start,
+                                    modifier = Modifier
+                                        .padding(8.dp)
+                                )
+                                Text(
+                                    text = "Subjects : ${String.subjects.replace("[","").replace("]","")}",
+                                    fontSize = 16.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    textAlign = TextAlign.Start,
+                                    modifier = Modifier
+                                        .padding(8.dp)
+                                )
+                            }
+                        }
+
+                    }
                 }
-            }
-            FloatingActionButton(modifier = Modifier
-                .align(Alignment.BottomEnd)
-                .padding(16.dp),
-                backgroundColor = Color.Blue,
-                onClick = {
-                    navController.navigate(Screen.FormScreen.route)
-                }) {
-                Icon(Icons.Filled.Add,"Add Student Data", tint = Color.White)
+                FloatingActionButton(modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(16.dp),
+                    backgroundColor = Color.Blue,
+                    onClick = {
+                        navController.navigate(Screen.FormScreen.route)
+                    }) {
+                    Icon(Icons.Filled.Add,"Add Student Data", tint = Color.White)
+                }
             }
         }
     }
